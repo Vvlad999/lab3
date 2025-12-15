1b73b4bb258b0911cc9b91977d88eca657e5960b
vlad <vlad4566777@gmail.com>
2025-12-16 01:08:11 +0700

    добавлен GameEngine для запуска цикла игры

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,11 +1,28 @@
-﻿namespace lab3
+﻿using lab3.Services;
+using lab3.Interfaces;
+using lab3.Core;
+
+namespace lab3
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
-           
-            
+            string txtPath = @"C:\Users\vlad4\OneDrive\Рабочий стол\textGame.txt";
+            string jsonPath = "result.json";
+
+            var converter = new TxtToGameConverter();
+            converter.ConvertAndSave(txtPath, jsonPath, "Игра");
+            IGameUI ui = new ConsoleGameUI();
+            IGameLoader loader = new JsonGameLoader(jsonPath);
+
+            var engine = new GameEngine(ui, loader);
+
+            engine.Run();
+
+            Console.ReadKey();
         }
     }
 }
